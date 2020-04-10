82a7ef6227251659dd6938f56a1d1a49733ffe26
Clay Whipp <claywhipp@gmail.com>
2020-04-09 19:57:31 -0500

    Edited HW7

--- Clay's code/HW8/HW8.m
+++ Clay's code/HW8/HW8.m
@@ -11,4 +11,5 @@
 syms b c
 
 % use a_z to find throttle vector? 
-[b; c] = [state_est_data(1:800,10) T_k-1]*state_est_data(2:801,10) 
+% state est data best data to use? 
+[b; c] = [state_est_data(1:800,10) T_k-1]^-1*state_est_data(2:801,10);
