b53565d363f28a4eb7a24177a803fa647ab949f4
Clay Whipp <claywhipp@gmail.com>
2020-04-23 19:31:55 -0500

    Jumpchecker is working now

--- Clay's code/HW7/HW7_v2/HW7_v2_matlab.m
+++ Clay's code/HW7/HW7_v2/HW7_v2_matlab.m
@@ -187,7 +187,7 @@ M_inv = inv(M_motor); %used to go from n1,...,n4 to TEAR
 % D_neweval = eval(subs(D_new, {u, v, w, p, q, r, phi, theta, psi, XNED, YNED, ZNED, n1, n2, n3, n4}, ...
 %              {0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 299, 299, 299, 299}));
 
-%% Q and R
+%% Q and R (covariances for Kalman Filter)
 q1_noise = 0.0101; %cov(wdot);
 r1_noise = 0.0166; %cov(z);
 
@@ -361,6 +361,69 @@ tau2 = eval(max(solve(1/sqrt((tau2*2*pi*10)^2+1^2) == 1/sqrt(2), tau2)));
 tau1 = 30e-3; % delay time of actuator 
 
 
+
+
+%% Ethan's code for his Kalman/Wiener filters IS BELOW
+
+%% Load in data for Covariance calculations
+load('In_out_data')
+syms t
+Ts = 0.005;
+
+%% Assert arbitrary power for noises
+measurement_noise = 0.025;
+process_noise = 0.05;
+
+%% filter cutoff for low pass filter 
+omega_c = 2*pi *20; %20 Hz (picked from experimentation, feel free to adjust)
+
+%% Calculating Dynamics for Kalman/Wiener Filters
+% Single Integrator state-space model
+A_single = [0];
+B_single = [1];
+C_single = [1];
+D_single = [0];
+
+% Double Integrator state-space model
+A_double = [0 1; 0 0];
+B_double = [0; 1]; 
+C_double = [1 0];
+D_double = [0];
+
+% Calculating Discrete Dynamics
+Ad_single = expm(A_single*t);
+Bd_single = int(Ad_single)*B_single;
+Ad_double = expm(A_double*t);
+Bd_double = int(Ad_double)*B_double;
+
+% Evalute Discrete Dynamics for t = Ts (sampling time)
+A_s_single = eval(subs(Ad_single,Ts));
+B_s_single = eval(subs(Bd_single,Ts));
+A_s_double = eval(subs(Ad_double,Ts));
+B_s_double = eval(subs(Bd_double,Ts));
+
+
+%% Covariance Calculations for Kalman/Wiener Filters
+% Calculate Covariances 
+q_single = cov(out.In_Out_Data{1}.Values.Data)*10000000000000; % input data
+r_single = cov(out.In_Out_Data{2}.Values.Data); % output data
+
+% don't worry about size of Q, the lqe function handle it
+q_double = q_single;
+r_double = r_single;
+
+% Wiener Filter code
+G_single = B_single; % noise input is same as process input
+G_double = B_double;
+
+
+%% LQE Gain for Wiener Filter
+% Gain for the Wiener filter
+L_single = lqe(A_single, G_single, C_single, q_single, r_single, 0);
+L_double = lqe(A_double, G_double, C_double, q_double, r_double, 0);
+% 
+% [v,lambda] = eig(A_s_single-L_single*C_single)
+% [v,lambda] = eig(A_s_double-L_double*C_double)
 
 
 
